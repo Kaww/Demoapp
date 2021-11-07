f12a97420fdedee972aec812427cbbd64c163ad1
Kaww <rmluux@gmail.com>
2021-11-07 14:20:28 +0100

    Push empty project

---
+++ Demoapp/DemoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoappApp.swift
+//  Demoapp
+//
+//  Created by KAWRANTIN LE GOFF on 07/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
