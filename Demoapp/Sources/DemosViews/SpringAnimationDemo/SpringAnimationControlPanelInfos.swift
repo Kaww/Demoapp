ecc2c2ff141d72cf5e0a16a8069fc8fc0d3d6590
Kaww <rmluux@gmail.com>
2021-11-13 18:43:52 +0100

    Adding Spring animations infos

---
+++ Demoapp/Sources/DemosViews/SpringAnimationDemo/SpringAnimationControlPanelInfos.swift
@@ -0,0 +1,20 @@
+//
+//  SpringAnimationControlPanelInfos.swift
+//  Demoapp
+//
+//  Created by KAWRANTIN LE GOFF on 13/11/2021.
+//
+
+import SwiftUI
+
+struct SpringAnimationControlPanelInfos: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpringAnimationControlPanelInfos_Previews: PreviewProvider {
+    static var previews: some View {
+        SpringAnimationControlPanelInfos()
+    }
+}
